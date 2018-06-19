c7e094fd8fdcb05054a1d234c482bddb607d8a77
Greg Horne <greghorne@hotmail.com>
2018-06-19 20:54:45 +0000

    corrected middleware error message

--- config/application.rb
+++ config/application.rb
@@ -32,11 +32,13 @@ module Code
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do 
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get]
-      end
-    end
+    config.middleware.use Rack::Attack
+
+    # config.middleware.insert_before 0, "Rack::Cors" do 
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get]
+    #   end
+    # end
   end
 end
