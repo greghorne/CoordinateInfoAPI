ef33eab68cb5134570c2db5bfbc814f3ccb99fa7
Greg Horne <greghorne@hotmail.com>
2018-06-26 16:28:01 +0000

    updated files trying to get -e production to work

---
+++ vendor/bundle/ruby/2.5.0/gems/rack-attack-5.3.2/lib/rack/attack/store_proxy/redis_store_proxy.rb
@@ -0,0 +1,48 @@
+require 'delegate'
+
+module Rack
+  class Attack
+    module StoreProxy
+      class RedisStoreProxy < SimpleDelegator
+        def self.handle?(store)
+          defined?(::Redis::Store) && store.is_a?(::Redis::Store)
+        end
+
+        def initialize(store)
+          super(store)
+        end
+
+        def read(key)
+          get(key, raw: true)
+        rescue Redis::BaseError
+        end
+
+        def write(key, value, options = {})
+          if (expires_in = options[:expires_in])
+            setex(key, expires_in, value, raw: true)
+          else
+            set(key, value, raw: true)
+          end
+        rescue Redis::BaseError
+        end
+
+        def increment(key, amount, options = {})
+          count = nil
+
+          pipelined do
+            count = incrby(key, amount)
+            expire(key, options[:expires_in]) if options[:expires_in]
+          end
+
+          count.value if count
+        rescue Redis::BaseError
+        end
+
+        def delete(key, _options = {})
+          del(key)
+        rescue Redis::BaseError
+        end
+      end
+    end
+  end
+end
---
+++ vendor/bundle/ruby/2.5.0/gems/rack-attack-5.3.2/lib/rack/attack/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  class Attack
+    VERSION = '5.3.2'
+  end
+end
